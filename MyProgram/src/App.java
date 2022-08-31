a4b1bc126534a3c099215068d3b1011658365db7
EurisG <euris809@gmail.com>
2022-08-31 10:33:46 -0400

    dice rolls from 1-6

--- MyProgram/src/App.java
+++ MyProgram/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
        
         Random r = new Random();
-        int num = r .nextInt(6);
+        int num = r .nextInt(6) + 1;
         System.out.println("You rolled a : " + num + " !");
     }
 }
